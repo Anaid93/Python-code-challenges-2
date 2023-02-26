db1859017bfd635200ccd6c2c0d801d592f133a4
Anaid <123415126+Anaid93@users.noreply.github.com>
2023-02-26 12:06:38 -0500

    Update challenge14.py

--- challenge14.py
+++ challenge14.py
@@ -1,5 +1,5 @@
 '''Define a function superposition() that takes two lists 
-and returns True if they have at least 1 member in common or 
+and returns True if they have at least 1 element in common or 
 returns False otherwise. Write the function using nested loop.'''
 
 def superposition(list_1:list, list_2:list):
